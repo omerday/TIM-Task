f653b47fa8d888705b806b36a90c8c9801748309
Omer <omer.dayan4u@gmail.com>
2024-02-23 16:24:57 +0200

    TIM-1 Medoc API

---
+++ medoc_api/commands/m_getstatusTCU_command.py
@@ -0,0 +1,23 @@
+import enums
+from commands import m_command, m_message
+import logging
+
+from commands.m_command import command
+
+logger = logging.getLogger(__name__)
+
+
+class get_status_TCU_command(m_command.command):
+    def __init__(self):
+        # super(command, self).__init__()
+        m_command.command.__init__(self)
+        self.response = None
+        self.command_id = enums.COMMAND_ID.GetStatusTCU
+
+    def send_message(self):
+        # command.send_message(self)
+        logger.info(str(self))
+        pass
+
+    def __str__(self):
+        return f'\t{command.__str__(self)}'
